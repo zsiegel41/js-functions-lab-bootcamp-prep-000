6d4fdde1ef86287f5abfc3d7baf9ee5a807cf708
Zach Siegel <zachsiegel41@gmail.com>
2017-09-10 03:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
